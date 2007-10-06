c230011976f99c96328e74b4aebd652ba24f7e59
jestabro <devnull@localhost>
2007-10-06 00:36:50 +0000

    changed obsolete reference to "oldTID" to "thread->mTID"

--- compat/Thread.c
+++ compat/Thread.c
@@ -202,7 +202,7 @@ void thread_stop( struct thread_Settings* thread ) {
 #if   defined( HAVE_POSIX_THREAD )
             // Cray J90 doesn't have pthread_cancel; Iperf works okay without
 #ifdef HAVE_PTHREAD_CANCEL
-            pthread_cancel( oldTID );
+            pthread_cancel( thread->mTID );
 #endif
 #else // Win32
             // this is a somewhat dangerous function; it's not

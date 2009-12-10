a067fb27c2843d27f767b691763a5ee5dc200d4d
AaronMatthewBrown <devnull@localhost>
2009-12-10 14:20:48 +0000

    Modify auto* scripts to handle the different uuid libraries/headers.

--- src/uuid.c
+++ src/uuid.c
@@ -1,11 +1,18 @@
+#include "config.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include<string.h>
-#if defined(__FreeBSD__)
+
+#if defined(HAVE_UUID_H)
+#warning DOING SOMETHING
 #include <uuid.h>
-#else
+#elif defined(HAVE_UUID_UUID_H)
 #include <uuid/uuid.h>
+#else
+#error No uuid header file specified
 #endif
+
 
 /* XXX: this code is not portable: not all versions of linux install libuuidgen
 	by default
@@ -21,13 +28,15 @@ get_uuid(char *temp)
     char     *s;
     uuid_t    uu;
 
-#if defined(__FreeBSD__)
+#if defined(HAVE_UUID_CREATE)
     uuid_create(&uu, NULL);
     uuid_to_string(&uu, &s, 0);
-#else
+#elif defined(HAVE_UUID_GENERATE)
     s = (char *) malloc(37);
     uuid_generate(uu);
     uuid_unparse(uu, s);
+#else
+#error No uuid function specified
 #endif
     memcpy(temp, s, 37);
 }

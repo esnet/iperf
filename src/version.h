7639c1a631089bd7a1e5e0aabfdf7dde3809fb02
Jef Poskanzer <jef@mail.acme.com>
2013-10-28 17:14:25 -0700

    Bump version to RC1.

--- src/version.h
+++ src/version.h
@@ -7,5 +7,5 @@
  * for complete information.
  */
 
-#define IPERF_VERSION "3.0-BETA7"
-#define IPERF_VERSION_DATE "14 September 2013"
+#define IPERF_VERSION "3.0-RC1"
+#define IPERF_VERSION_DATE "28 October 2013"

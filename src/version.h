578a1961cff7c97a7f3d57837b369b4995060d73
Jef Poskanzer <jef@mail.acme.com>
2013-11-01 14:01:20 -0700

    Bump version to 3.0-RC3.

--- src/version.h
+++ src/version.h
@@ -7,5 +7,5 @@
  * for complete information.
  */
 
-#define IPERF_VERSION "3.0-RC2"
-#define IPERF_VERSION_DATE "29 October 2013"
+#define IPERF_VERSION "3.0-RC3"
+#define IPERF_VERSION_DATE "01 November 2013"

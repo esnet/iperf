979405373d872ed08238f279453561c131963264
Jef Poskanzer <jef@mail.acme.com>
2013-11-25 09:57:50 -0800

    When receiving a disk file with the -F flag, do an fsync() after each write().

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -2105,8 +2105,10 @@ diskfile_recv(struct iperf_stream *sp)
     int r;
 
     r = sp->rcv2(sp);
-    if (r > 0)
+    if (r > 0) {
 	(void) write(sp->diskfile_fd, sp->buffer, r);
+	(void) fsync(sp->diskfile_fd);
+    }
     return r;
 }
 

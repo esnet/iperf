49f74e582c8786f4fc0e044c301a05d5791b2ff0
Bruce A. Mah <bmah@es.net>
2014-01-10 13:22:11 -0800

    Prepare for iperf3 3.0.1.

--- src/version.h
+++ src/version.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009-2011, The Regents of the University of California,
+ * Copyright (c) 2009-2014, The Regents of the University of California,
  * through Lawrence Berkeley National Laboratory (subject to receipt of any
  * required approvals from the U.S. Dept. of Energy).  All rights reserved.
  *
@@ -7,5 +7,5 @@
  * for complete information.
  */
 
-#define IPERF_VERSION "3.0-RC5"
-#define IPERF_VERSION_DATE "07 November 2013"
+#define IPERF_VERSION "3.0.1"
+#define IPERF_VERSION_DATE "10 January 2014"

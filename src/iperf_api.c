47c1bdbfd653bbfb7e1309829f3f0318e0de34d7
Bruce A. Mah <bmah@es.net>
2014-01-21 12:59:47 -0800

    Fix an abort observed on MacOS X 10.9 (but applicable to other platforms).
    mapped_v4_to_regular_v4() committed the sin of doing strcpy(3) on
    overlapping buffers.  This caused an abort on MacOS X 10.9.  Fix this
    to use memcpy(3) instead, which handles overlapping buffers correctly.
    
    Fixes #135 (Crash on OS X when using IP address)
    (cherry picked from commit 723f2f7b2aed8ee06f081575bd888751f556b5d7)
    Signed-off-by: Bruce A. Mah <bmah@es.net>

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -413,8 +413,10 @@ mapped_v4_to_regular_v4(char *str)
     int prefix_len;
 
     prefix_len = strlen(prefix);
-    if (strncmp(str, prefix, prefix_len) == 0)
-        strcpy(str, str+prefix_len);
+    if (strncmp(str, prefix, prefix_len) == 0) {
+	int str_len = strlen(str);
+	memmove(str, str + prefix_len, str_len - prefix_len + 1);
+    }
 }
 
 void

58fd2c37fce88f78209278981f8f0d929162e6da
Fredrik Fornwall <fredrik@fornwall.net>
2014-06-16 09:02:44 +0200

    in6_flowlabel_req is already defined on Android

--- src/flowlabel.h
+++ src/flowlabel.h
@@ -19,6 +19,7 @@
    conflicts with "netinet/in.h" .
 */
 
+#ifndef __ANDROID__
 struct in6_flowlabel_req
 {
     struct in6_addr flr_dst;
@@ -31,6 +32,7 @@ struct in6_flowlabel_req
     __u32   __flr_pad;
     /* Options in format of IPV6_PKTOPTIONS */
 };
+#endif
 
 #define IPV6_FL_A_GET           0
 #define IPV6_FL_A_PUT           1

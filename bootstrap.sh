03a379dbe8f7ef30aaa21926261381dd7dcf6e5c
Bruce A. Mah <bmah@kitchenlab.org>
2014-06-20 08:34:34 -0700

    Merge pull request #188 from i2aaron/master
    Update the license in the bootstrap.sh
    (cherry picked from commit 4fe55f366737bc9c51599380fd93efa2aee34ae5)
    
    Signed-off-by: Bruce A. Mah <bmah@es.net>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 #
-#      $Id: bootstrap.sh 446 2008-04-21 04:07:15Z boote $
-#
 #########################################################################
 #									#
 #			   Copyright (C)  2003				#
 #	     			Internet2				#
-#			   All Rights Reserved				#
+#                                                                       #
+#  Licensed under the Apache License, Version 2.0 (the "License");      #
+#  you may not use this file except in compliance with the License.     #
+#  You may obtain a copy of the License at                              #
+#                                                                       #
+#  http://www.apache.org/licenses/LICENSE-2.0                           #
+#                                                                       #
+#  Unless required by applicable law or agreed to in writing, software  #
+#  distributed under the License is distributed on an "AS IS" BASIS,    #
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      #
+#  implied. See the License for the specific language governing         #
+#  permissions and limitations under the License.                       #
 #									#
 #########################################################################
 #

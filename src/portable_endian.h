1f29ffe70d789f9568a05e3625cbdcbc616407c6
Bruce A. Mah <bmah@es.net>
2014-09-15 15:09:50 -0700

    Fix build on OpenBSD (tested on 5.4).
    Breakage was introduced with commits for Issue #191.

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -42,6 +42,15 @@
 
 #	include <sys/endian.h>
 
+#	define be16toh(x) betoh16(x)
+#	define le16toh(x) letoh16(x)
+
+#	define be32toh(x) betoh32(x)
+#	define le32toh(x) letoh32(x)
+
+#	define be64toh(x) betoh64(x)
+#	define le64toh(x) letoh64(x)
+
 #elif defined(__NetBSD__) || defined(__FreeBSD__) || defined(__DragonFly__)
 
 #	include <sys/endian.h>

738c4dc68f3355a0133f8de79ff2340e47b65f91
Bruce A. Mah <bmah@es.net>
2014-09-19 15:11:02 -0700

    Make this work on FreeBSD.
    From my understanding this should make it work on NetBSD and Dragonfly
    as well.
    
    Part of #191.

--- src/portable_endian.h
+++ src/portable_endian.h
@@ -55,15 +55,6 @@
 
 #	include <sys/endian.h>
 
-#	define be16toh(x) betoh16(x)
-#	define le16toh(x) letoh16(x)
-
-#	define be32toh(x) betoh32(x)
-#	define le32toh(x) letoh32(x)
-
-#	define be64toh(x) betoh64(x)
-#	define le64toh(x) letoh64(x)
-
 #elif defined(__SunOS)
 
 #	include <sys/types.h>

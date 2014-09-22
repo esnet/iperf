8365c3d0031c9813d64a8c64af2a9178f929cf3c
Bruce A. Mah <bmah@es.net>
2014-09-22 15:11:59 -0700

    Don't pass NULL when we really mean to pass 0.
    Fixes a warning on MacOS.

--- src/iperf_client_api.c
+++ src/iperf_client_api.c
@@ -271,7 +271,7 @@ iperf_connect(struct iperf_test *test)
     /* Create and connect the control channel */
     if (test->ctrl_sck < 0)
 	// Create the control channel using an ephemeral port
-	test->ctrl_sck = netdial(test->settings->domain, Ptcp, test->bind_address, NULL, test->server_hostname, test->server_port);
+	test->ctrl_sck = netdial(test->settings->domain, Ptcp, test->bind_address, 0, test->server_hostname, test->server_port);
     if (test->ctrl_sck < 0) {
         i_errno = IECONNECT;
         return -1;

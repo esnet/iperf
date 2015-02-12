081ad7c08ca1401938f069b8bfeeae9e2f8a6a0b
Havard Eidnes <he@uninett.no>
2015-02-12 14:13:07 +0100

    Add casts for the two remaining tolower() uses, since tolower() is undefined for negative character values (except for -1).
    Also simplify the existing casts for tolower() usages.

--- src/cjson.c
+++ src/cjson.c
@@ -58,10 +58,10 @@ static int cJSON_strcasecmp( const char *s1, const char *s2 )
 		return ( s1 == s2 ) ? 0 : 1;
 	if ( ! s2 )
 		return 1;
-	for ( ; tolower(*s1) == tolower(*s2); ++s1, ++s2)
+	for ( ; tolower((u_char)*s1) == tolower((u_char)*s2); ++s1, ++s2)
 		if( *s1 == 0 )
 			return 0;
-	return tolower(*(const unsigned char *)s1) - tolower(*(const unsigned char *)s2);
+	return tolower((u_char)*s1) - tolower((u_char)*s2);
 }
 
 

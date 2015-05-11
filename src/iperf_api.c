7ee977c6c60527ce5959193dc422964a44ab626f
Bruce A. Mah <bmah@es.net>
2015-05-11 10:44:06 -0700

    Make --get-server-output properly include all of the server side output.
    Fixes #251, with a bug fix based on diagnosis and analysis from @nomel.
    
    (cherry picked from commit 582438111107ad3fce171afaee277bf594099ef5)
    Signed-off-by: Bruce A. Mah <bmah@es.net>

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -2218,6 +2218,7 @@ iperf_reporter_callback(struct iperf_test *test)
             /* print interval results for each stream */
             iperf_print_intermediate(test);
             break;
+        case TEST_END:
         case DISPLAY_RESULTS:
             iperf_print_intermediate(test);
             iperf_print_results(test);

ad2a706fd194381c9a1f571b93d16c3edbd4d114
Nevo Hed <nhed+github@projectdecibel.com>
2016-10-17 16:25:07 -0400

    Replace iprintf w/ iperf_printf (cygwin conflict) (#468)
    This does not seek to address all cygwin portability issues
    just this specific compile-time conflict
    
    See also #280 and #295.
    See https://github.com/esnet/iperf/issues/280

--- src/iperf_api.h
+++ src/iperf_api.h
@@ -260,7 +260,7 @@ int iperf_setaffinity(struct iperf_test *, int affinity);
 int iperf_clearaffinity(struct iperf_test *);
 
 /* Custom printf routine. */
-int iprintf(struct iperf_test *test, const char *format, ...) __attribute__ ((format(printf,2,3)));
+int iperf_printf(struct iperf_test *test, const char *format, ...) __attribute__ ((format(printf,2,3)));
 int iflush(struct iperf_test *test);
 
 /* Error routines. */

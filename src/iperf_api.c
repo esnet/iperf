esnet/iperf

commit 1460aa3433769db1f5660e7819b669e5414e9b42

fix for https://github.com/esnet/iperf/issues/650 (#651)
Reset authentication token between tests.  Fixes #650.

src/iperf_api.c
@@ -2305,6 +2305,7 @@ iperf_reset_test(struct iperf_test *test)
     test->settings->burst = 0;
     test->settings->mss = 0;
     test->settings->tos = 0;
+    test->settings->authtoken = NULL;
     memset(test->cookie, 0, COOKIE_SIZE);
     test->multisend = 10;	/* arbitrary */
     test->udp_counters_64bit = 0;

commit 393d0b2f9be9e10212dc2136cd87fc37efc7dda0

Fix memory leak introduced in 1460aa3433769db1f5660e7819b669e5414e9b42.
Pointed out by:  @pprindeville
Should have been found by:  @bmah888

src/iperf_api.c
@@ -2305,7 +2305,10 @@ iperf_reset_test(struct iperf_test *test)
     test->settings->burst = 0;
     test->settings->mss = 0;
     test->settings->tos = 0;
-    test->settings->authtoken = NULL;
+    if (test->settings->authtoken) {
+	free(test->settings->authtoken);
+	test->settings->authtoken = NULL;
+    }
     memset(test->cookie, 0, COOKIE_SIZE);
     test->multisend = 10;	/* arbitrary */
     test->udp_counters_64bit = 0;
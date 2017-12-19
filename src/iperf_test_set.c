a16f723ec95112e61eb2d227cf1bf9cbe02e2928
Boris Okunev <hait.08@mail.ru>
2017-12-19 12:34:06 +0300

    Added test run

--- src/iperf_test_set.c
+++ src/iperf_test_set.c
@@ -5,39 +5,70 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 #include "iperf_test_set.h"
 #include "iperf_api.h"
 
-int 
-ts_run_test(struct test_unit* tu)
+
+int
+ts_parse_args(struct test_unit* tu)
 {
-	char* host;
-	int port;
+	cJSON* options = cJSON_GetObjectItemCaseSensitive(tu->json_test_case, "options");
+	char str[] = "-t 10 -i 5";
+	char **argvs = NULL;
+	char *tmp = strtok(str, " ");
+	printf("parsing \n");
+	int count = 0, i = 0;
+
+	while (tmp)
+	{
+		argvs = realloc(argvs, sizeof(char *) * ++count);
+
+		if (argvs == NULL)
+			exit(-1);
+		argvs[count - 1] = tmp;
+
+		tmp = strtok(NULL, " ");
+	}
+
+	for (i = 0; i < (count); ++i)
+		printf("res[%d] = %s\n", i, argvs[i]);
+	tu->argcs = count;
+	tu->argvs = argvs;
+	return 0;
+}
+
+int 
+ts_run_test(struct test_unit* tu, struct iperf_test* main_test)
+{
 	struct iperf_test *child_test;
+	child_test = iperf_new_test();
 
-	//pid_t pid;
-	//int *shared; /* pointer to the shm */
-	//int shmid;
+	if (!child_test)
+		iperf_errexit(NULL, "create new test error - %s", iperf_strerror(i_errno));
+	iperf_defaults(child_test);	/* sets defaults */
 
-	//shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+	iperf_set_test_role(child_test, 'c'); 
+	iperf_set_test_server_hostname(child_test, main_test->server_hostname);
+	iperf_set_test_server_port(child_test, main_test->server_port);
+	iperf_set_test_json_output(child_test, 1);
 
-	iperf_set_test_role(child_test, 'c'); //take from parent's process or add to argv
-	iperf_set_test_server_hostname(child_test, host);
-	iperf_set_test_server_port(child_test, port);
+	ts_parse_args(tu);
 
-	iperf_parse_arguments(child_test, tu->op_count, tu->args);
+	iperf_parse_arguments(child_test, tu->argcs, tu->argvs);
 
 	if (iperf_run_client(child_test) < 0) {
-		//fprintf(stderr, "%s: error - %s\n", argv0, iperf_strerror(i_errno));
 		exit(EXIT_FAILURE);
 	}
+	else 		printf("Running clint \n");
 
 	if (iperf_get_test_json_output_string(child_test)) {
-		fprintf(iperf_get_test_outfile(child_test), "%zd bytes of JSON emitted\n",
-			strlen(iperf_get_test_json_output_string(child_test)));
+		printf("%s\n", iperf_get_test_json_output_string(child_test));
 	}
+
+	printf("Finished \n");
 	return 0;
 }
 
@@ -50,9 +81,13 @@ ts_run_bulk_test(struct iperf_test* test)
 	char *str;
 	FILE * inputFile = fopen(test->test_set_file, "r");
 	cJSON *json;
+	cJSON *node;
 
 	if (!inputFile)
+	{ 
+		printf("File is not exist");
 		return -1;
+	}
 	else
 	{
 		fseek(inputFile, 0, SEEK_END);
@@ -60,25 +95,62 @@ ts_run_bulk_test(struct iperf_test* test)
 		fseek(inputFile, 0, SEEK_SET);
 	}
 
+	//creating json file
 	str = malloc(size + 1);
 	fread(str, size, 1, inputFile);
 	str[size] = '\n';
 
 	json = cJSON_Parse(str);
 
-	pid_t pid;
-	int *shared; /* pointer to the shm */
+	fclose(inputFile);
+	free(str);
 
-	t_set.res = 0;
-	t_set.test_count = 0;
-	//t_set.path = strdup(path);
 
-	
+	//test counting
+	node = json->child;
+
+	i = 0;
+
+	do
+	{
+		++i;
+		node = node->next;
+	} while (node);
+
+	t_set.test_count = i;
+
+
+
+	printf("%s\n", cJSON_Print(json));
+
+	printf("%d \n", i);
+
+	//parsing
+	t_set.suite = malloc(sizeof(struct test_unit*) * i);
+
+	node = json->child;
+
 	for (i = 0; i < t_set.test_count; ++i)
 	{
-		//ts_run_test(t_set->&suite[i]);
+		struct test_unit* unit = malloc(sizeof(struct test_unit));
+		unit->id = i;
+		unit->json_test_case = node;
+		t_set.suite[i] = unit;
+		node = node->next;
 	}
 
-	//free(t_set.path);
+	for (i = 0; i < t_set.test_count; ++i)
+	{
+		ts_run_test(t_set.suite[i], test);
+	}
+
+
+
+	/*delete argvs*/
+
+
+	cJSON_Delete(json);
+	free(t_set.suite);
+
 	return 0; //add correct completion of the test to the errors(?)
 }
--- src/iperf_test_set.h
+++ src/iperf_test_set.h
@@ -12,8 +12,8 @@ struct iperf_test;
 struct test_unit
 {
     int       id;
-	int       op_count;
-	char	  **args;
+	int       argcs;
+	char	  **argvs;
     char      *json_output_string;
     cJSON     *json_test_case;
     cJSON     *json_output;
@@ -24,12 +24,12 @@ struct test_set
 	int res;
 	int test_count;
 	char *path;
-	struct test_unit *suite;
+	struct test_unit **suite;
 };
 
+int ts_parse_args(struct test_unit* tu);
 
-
-int ts_run_test(struct test_unit* tu);
+int ts_run_test(struct test_unit* tu, struct iperf_test* main_test);
 
 int ts_run_bulk_test(struct iperf_test* test);
 

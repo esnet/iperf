a5e327b442368eff7faf5093c66c52c13dbf1edf
Suresh Sundriyal <sureshsundriyal@gmail.com>
2018-01-03 12:00:27 -0800

    Include signal.h + time.h. (#680)
    Required to avoid implicit declarations of 'signal()' and 'time()'; which
    causes compilation to error out while compiling a statically linked binary.

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -34,6 +34,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <getopt.h>
 #include <errno.h>
 #include <signal.h>
--- src/iperf_util.c
+++ src/iperf_util.c
@@ -32,6 +32,7 @@
 #include "iperf_config.h"
 
 #include <stdio.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>

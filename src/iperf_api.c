3aa196130a26b31e40266a632c15346ed397559d
Ben Greear <greearb@candelatech.com>
2019-10-28 10:48:16 -0700

    Fix reporting peer's results in server mode when server is sender.
    Signed-off-by: Ben Greear <greearb@candelatech.com>

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -3359,13 +3359,7 @@ iperf_print_results(struct iperf_test *test)
                                                                 (double) receiver_time, (double) end_time, (int64_t) bytes_received,
                                                                 bandwidth * 8, stream_must_be_sender));
                     else
-                        if (test->role == 's' && sp->sender) {
-                            if (test->verbose)
-                                iperf_printf(test, report_receiver_not_available_format, sp->stream_id);
-                        }
-                        else {
-                            iperf_printf(test, report_bw_format, sp->stream_id, mbuf, start_time, receiver_time, ubuf, nbuf, report_receiver);
-                        }
+                        iperf_printf(test, report_bw_format, sp->stream_id, mbuf, start_time, receiver_time, ubuf, nbuf, report_receiver);
                 }
                 else {
                     /*
@@ -3381,13 +3375,9 @@ iperf_print_results(struct iperf_test *test)
                             lost_percent = 0.0;
                         }
 
-                        if (test->role == 's' && sp->sender) {
-                            if (test->verbose)
-                                iperf_printf(test, report_receiver_not_available_format, sp->stream_id);
-                        }
-                        else {
-                            iperf_printf(test, report_bw_udp_format, sp->stream_id, mbuf, start_time, receiver_time, ubuf, nbuf, sp->jitter * 1000.0, (sp->cnt_error - sp->omitted_cnt_error), (receiver_packet_count - sp->omitted_packet_count), lost_percent, report_receiver);
-                        }
+                        iperf_printf(test, report_bw_udp_format, sp->stream_id, mbuf, start_time, receiver_time, ubuf, nbuf,
+                                     sp->jitter * 1000.0, (sp->cnt_error - sp->omitted_cnt_error),
+                                     (receiver_packet_count - sp->omitted_packet_count), lost_percent, report_receiver);
                     }
                 }
             }
--- src/iperf_server_api.c
+++ src/iperf_server_api.c
@@ -203,7 +203,6 @@ iperf_handle_message_server(struct iperf_test *test)
                 iclosesocket(sp->socket, test);
                 sp->socket = -1;
             }
-            test->reporter_callback(test);
 	    if (iperf_set_send_state(test, EXCHANGE_RESULTS) != 0)
                 return -1;
             if (iperf_exchange_results(test) < 0)
@@ -212,6 +211,7 @@ iperf_handle_message_server(struct iperf_test *test)
                 return -1;
             if (test->on_test_finish)
                 test->on_test_finish(test);
+            test->reporter_callback(test);
             break;
         case IPERF_DONE:
             break;

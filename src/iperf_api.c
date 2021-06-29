87ac4e226326a559e14963d125b3cf95be7d824b
Bruce A. Mah <bmah@es.net>
2021-06-29 12:06:13 -0700

    Remove fsync(2) call after every write to a receiving --file.
    This removes a performance pessimization that wasn't really
    needed in the first place.
    
    Fixes #1159.

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -4277,7 +4277,6 @@ diskfile_recv(struct iperf_stream *sp)
     r = sp->rcv2(sp);
     if (r > 0) {
 	(void) write(sp->diskfile_fd, sp->buffer, r);
-	(void) fsync(sp->diskfile_fd);
     }
     return r;
 }

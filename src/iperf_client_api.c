0b15c449892db70990270d7606d3d8a9116213ab
Sarah Larsen <swlarsen@es.net>
2021-09-22 18:54:39 -0700

    Add comment to clean_up_and_fail

--- src/iperf_client_api.c
+++ src/iperf_client_api.c
@@ -656,6 +656,8 @@ iperf_run_client(struct iperf_test * test)
         cJSON_AddStringToObject(test->json_top, "error", iperf_strerror(i_errno));
         iperf_json_finish(test);
         iflush(test);
+        // Return 0 and not -1 since all terminating function were done here.
+        // Also prevents error message logging outside the already closed JSON output.
         return 0;
     }
     iflush(test);

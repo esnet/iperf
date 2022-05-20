92f0a230bb7b4f8f828d9c5f91350ce56eca8c8c
Jesse Gorzinski <jgorzinski@gmail.com>
2022-05-20 13:07:41 -0500

    beef up IBM i daemon() replacement

--- src/main.c
+++ src/main.c
@@ -41,35 +41,50 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
-
 #include "iperf.h"
 #include "iperf_api.h"
 #include "iperf_util.h"
 #include "iperf_locale.h"
+#include "queue.h"
 #include "net.h"
 #include "units.h"
 
 #ifdef __PASE__
-static int pase_daemon()
+#include <fcntl.h>
+#include <unistd.h>
+static int pase_daemon(int nochdir, int noclose)
 {
-    switch (fork()) {
-    case -1:
-        return -1;
-
-    case 0:
-        break;
-
-    default:
-        exit(0);
-    }
-
-    if (setsid() == -1) {
+    if(-1 == fork()) {
         return -1;
     }
 
-    return umask(0);
+    if (-1 == setsid()) {
+        return -1;
+    }
+
+    if (-1 == umask(0)) {
+        return -1;
+    }
+    int devnull = open("/dev/null", O_RDWR);
+    if (-1 == devnull) {
+        return -1;
+    }
+    if(0 == nochdir) {
+        if (chdir("/") == -1) {
+            return -1;
+        }
+    }
+    if(0 != noclose) {
+        return 0;
+    }
+    for(int i=0; i<=3;++i) {
+        if (-1 == dup2(devnull, i)) {
+            return -1;
+        }
+    }
+    return 0;
 }
-#define daemon(x,r) pase_daemon()
+#define daemon(x,r) pase_daemon(x,r)
 #endif
 
 static int run(struct iperf_test *test);

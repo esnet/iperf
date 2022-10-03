76138bd6588c89e4ef1be92e72c285859906e90d
Albert <67727631+CodingAlbert@users.noreply.github.com>
2022-10-03 02:06:56 +0200

    Change unit display bits/s -> bit/s

--- src/iperf_locale.c
+++ src/iperf_locale.c
@@ -376,31 +376,31 @@ const char report_bw_format[] =
 "[%3d]%s %6.2f-%-6.2f s  %ss  %ss/s                  %s\n";
 
 const char report_bw_retrans_format[] =
-"[%3d]%s %6.2f-%-6.2f s  %ss  %ss/s  %3u             %s\n";
+"[%3d]%s %6.2f-%-6.2f s  %ss  %s/s  %3u             %s\n";
 
 const char report_bw_retrans_cwnd_format[] =
-"[%3d]%s %6.2f-%-6.2f s  %ss  %ss/s  %3u   %ss       %s\n";
+"[%3d]%s %6.2f-%-6.2f s  %ss  %s/s  %3u   %ss       %s\n";
 
 const char report_bw_udp_format[] =
-"[%3d]%s %6.2f-%-6.2f s  %ss  %ss/s  %5.3f ms  %d/%d (%.2g%%)  %s\n";
+"[%3d]%s %6.2f-%-6.2f s  %ss  %s/s  %5.3f ms  %d/%d (%.2g%%)  %s\n";
 
 const char report_bw_udp_sender_format[] =
-"[%3d]%s %6.2f-%-6.2f s  %ss  %ss/s %s %d  %s\n";
+"[%3d]%s %6.2f-%-6.2f s  %ss  %s/s %s %d  %s\n";
 
 const char report_summary[] =
 "Test Complete. Summary Results:\n";
 
 const char report_sum_bw_format[] =
-"[SUM]%s %6.2f-%-6.2f s  %ss  %ss/s                  %s\n";
+"[SUM]%s %6.2f-%-6.2f s  %ss  %s/s                  %s\n";
 
 const char report_sum_bw_retrans_format[] =
-"[SUM]%s %6.2f-%-6.2f s  %ss  %ss/s  %3d             %s\n";
+"[SUM]%s %6.2f-%-6.2f s  %ss  %s/s  %3d             %s\n";
 
 const char report_sum_bw_udp_format[] =
-"[SUM]%s %6.2f-%-6.2f s  %ss  %ss/s  %5.3f ms  %d/%d (%.2g%%)  %s\n";
+"[SUM]%s %6.2f-%-6.2f s  %ss  %s/s  %5.3f ms  %d/%d (%.2g%%)  %s\n";
 
 const char report_sum_bw_udp_sender_format[] =
-"[SUM]%s %6.2f-%-6.2f s  %ss  %ss/s %s %d  %s\n";
+"[SUM]%s %6.2f-%-6.2f s  %ss  %s/s %s %d  %s\n";
 
 const char report_omitted[] = "(omitted)";
 

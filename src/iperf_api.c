622fa9f7279b93e5ffb1da47a3da3a7b1eaf2829
David Kim <kimdavid@microsoft.com>
2023-06-19 10:22:31 -0700

    undo uncessary changes

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -2407,14 +2407,14 @@ send_results(struct iperf_test *test)
 	    i_errno = IEPACKAGERESULTS;
 	    r = -1;
 	} else {
-		cJSON_AddItemToObject(j, "streams", j_streams);
+	    cJSON_AddItemToObject(j, "streams", j_streams);
 	    SLIST_FOREACH(sp, &test->streams, streams) {
             j_stream = cJSON_CreateObject();
             if (j_stream == NULL) {
                 i_errno = IEPACKAGERESULTS;
                 r = -1;
             } else {
-			cJSON_AddItemToArray(j_streams, j_stream);
+		    cJSON_AddItemToArray(j_streams, j_stream);
 			bytes_transferred = sp->sender ? (sp->result->bytes_sent - sp->result->bytes_sent_omit) : sp->result->bytes_received;
 			retransmits = (sp->sender && test->sender_has_retransmits) ? sp->result->stream_retrans : -1;
 			cJSON_AddNumberToObject(j_stream, "id", sp->id);

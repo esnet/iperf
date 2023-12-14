30ac90d59be1ba245f0def74243b1c1dd5e413d9
Maks Mishin <maks.mishinFZ@gmail.com>
2023-12-15 00:13:48 +0300

    Fix descriptor leak in some return paths

--- src/iperf_tcp.c
+++ src/iperf_tcp.c
@@ -309,6 +309,7 @@ iperf_tcp_listen(struct iperf_test *test)
 
         if (listen(s, INT_MAX) < 0) {
             i_errno = IESTREAMLISTEN;
+            close(s);
             return -1;
         }
 
@@ -329,6 +330,7 @@ iperf_tcp_listen(struct iperf_test *test)
     }
     if (test->settings->socket_bufsize && test->settings->socket_bufsize > sndbuf_actual) {
 	i_errno = IESETBUF2;
+    close(s);
 	return -1;
     }
 
@@ -346,6 +348,7 @@ iperf_tcp_listen(struct iperf_test *test)
     }
     if (test->settings->socket_bufsize && test->settings->socket_bufsize > rcvbuf_actual) {
 	i_errno = IESETBUF2;
+    close(s);
 	return -1;
     }
 

8a8286b5ba7e1fc55bd29591935532071fc4b13d
swlars <89053414+swlars@users.noreply.github.com>
2025-05-09 13:43:04 -0700

    Update iperf_api.h
    Fix errors

--- src/iperf_api.h
+++ src/iperf_api.h
@@ -434,7 +434,7 @@ enum {
     IEUDPFILETRANSFER = 34, // Cannot transfer file using UDP
     IESERVERAUTHUSERS = 35,   // Cannot access authorized users file
     IEDISKFILEZEROCOPY = 36, // Sending disk file using MSG_ZEROCOPY is not supported 
-    IECNTLKA = 36,          // Control connection Keepalive period should be larger than the full retry period (interval * count)
+    IECNTLKA = 37,          // Control connection Keepalive period should be larger than the full retry period (interval * count)
 
     /* Test errors */
     IENEWTEST = 100,        // Unable to create a new test (check perror)

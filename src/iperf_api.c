0f0fe61e9158dc8103502e7808dcb7e9e5caa82a
Dane Murphy <danem7@gmail.com>
2025-07-08 09:39:11 -0700

    man page entry

--- src/iperf_api.c
+++ src/iperf_api.c
@@ -3764,8 +3764,10 @@ iperf_print_intermediate(struct iperf_test *test)
      * results around unless we're the server and the client requested the server output.
      *
      * This avoids unneeded memory build up for long sessions.
+     * 
+     * The user can still opt in for all measurement data via the --json-stream-full-output option.
      */
-    discard_json = !test->json_stream_full_output && test->json_stream == 1 && !(test->role == 's' && test->get_server_output);
+    discard_json =  test->json_stream == 1 && !test->json_stream_full_output && !(test->role == 's' && test->get_server_output);
 
     if (test->json_output) {
         json_interval = cJSON_CreateObject();

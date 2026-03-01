72716d1654c682dbee1ced57b76cf7ed71b0c567
Leandru Martin <leandrumartin@gmail.com>
2026-02-28 22:42:13 -0600

    Revert "Correct file paths"
    This reverts commit 4b6560275f0532623404ddb3d1385c511eff75b5.

--- experiment/baseline_quic.py
+++ experiment/baseline_quic.py
@@ -1,6 +1,6 @@
 import subprocess
 
 subprocess.run(
-    "../src/iperf3 -c localhost -p 5201 --quic --json --logfile ./output/baseline_quic.log",
+    "./src/iperf3 -c localhost -p 5201 --quic --json --logfile ./output/baseline_quic.log",
     shell=True
 )
--- experiment/baseline_udp.py
+++ experiment/baseline_udp.py
@@ -1,6 +1,6 @@
 import subprocess
 
 subprocess.run(
-    "../src/iperf3 -c localhost -p 5201 --udp --json --logfile ./output/baseline_udp.log",
+    "./src/iperf3 -c localhost -p 5201 --udp --json --logfile ./output/baseline_udp.log",
     shell=True
 )

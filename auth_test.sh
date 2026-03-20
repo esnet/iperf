65900028406f39a9081eeecf5aa0350da7e4c8ad
Sarah Larsen <swlarsen@es.net>
2026-03-20 18:48:20 +0000

    Update to not have arguments

--- auth_test.sh
+++ auth_test.sh
@@ -6,16 +6,13 @@
 # Be sure to test both client and server on Linux, BSD, and OSX
 #
 
-if [ $# -ne 2 ]
-then
-  echo "Usage: `basename "$0"` binary output"
-  exit $E_BADARGS
-fi
 #
 set -x
 
-binary=$1
-output=$2
+binary=./src/iperf3
+output=/tmp/iperf3_auth_tmp.txt
+
+# Generate keys and authorized user file
 
 #sudo apt-get install libssl-dev
 openssl genrsa -out private.pem 2048
@@ -28,12 +25,16 @@ SHA=$(echo -n "{$S_USER}$S_PASSWD" | sha256sum | awk '{ print $1 }')
 echo ${S_USER},${SHA} > authorized-user.txt
 cat authorized-user.txt
 
-./src/${binary} -s --rsa-private-key-path=./private.pem  --authorized-user=./authorized-user.txt &> ${output} &
+# Run Server
 
-IPERF3_PASSWORD=${S_PASSWD} ./src/${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
-IPERF3_PASSWORD="very incorrect" ./src/${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
-IPERF3_PASSWORD="very,very,very, incorrect, and really, really, really, really, really long. Just to check." ./src/${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
-IPERF3_PASSWORD=${S_PASSWD} ./src/${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
+${binary} -s --rsa-private-key-path=./private.pem  --authorized-user=./authorized-user.txt &> ${output} &
+
+# Run Client
+
+IPERF3_PASSWORD=${S_PASSWD} ${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
+IPERF3_PASSWORD="very incorrect" ${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
+IPERF3_PASSWORD="very,very,very, incorrect, and really, really, really, really, really long. Just to check." ${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
+IPERF3_PASSWORD=${S_PASSWD} ${binary} -c localhost --rsa-public-key-path=./public.pem --username=${S_USER}
 
 pkill ${binary}
 
